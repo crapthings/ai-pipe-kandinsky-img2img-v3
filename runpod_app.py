18e05484899f5774fd909c5ec7fb75fd685368d8
crapthings <crapthings@gmail.com>
2023-11-26 15:32:21 +0800

    fix code

--- runpod_app.py
+++ runpod_app.py
@@ -1,10 +1,12 @@
+import math
 import requests
 
 import torch
+from diffusers.utils import load_image
 import runpod
 
 from utils import extract_origin_pathname, upload_image
-from txt2img import txt2img
+from img2img import img2img
 
 def run (job, _generator = None):
     # prepare task
@@ -12,36 +14,48 @@ def run (job, _generator = None):
         print('debug', job)
 
         _input = job.get('input')
+
         debug = _input.get('debug')
+        input_url = _input.get('input_url')
         upload_url = _input.get('upload_url')
 
         prompt = _input.get('prompt', 'a dog')
         negative_prompt = _input.get('negative_prompt', '')
-        width = _input.get('width', 1024)
-        height = _input.get('height', 1024)
         num_inference_steps = _input.get('num_inference_steps', 50)
         guidance_scale = _input.get('guidance_scale', 7.0)
+        strength = _input.get('strength', 0.5)
         seed = _input.get('seed')
+
+        input_image = load_image(input_url).convert('RGB')
+
+        originalwidth, originalHeight = input_image.size
+
+        input_image = input_image.resize([1024, 1024])
 
         if seed is not None:
             _generator = torch.Generator(device = 'cuda').manual_seed(seed)
 
-        output_image = txt2img(
+        output_image = img2img(
+            image = input_image,
             prompt = prompt,
             negative_prompt = negative_prompt,
-            width = width,
-            height = height,
-            num_inference_steps = num_inference_steps,
+            num_inference_steps = math.ceil(num_inference_steps / strength),
             guidance_scale = guidance_scale,
+            strength = strength,
             generator = _generator
         ).images[0]
+
+        output_image = output_image.resize([originalwidth, originalHeight])
 
         if debug:
             output_image.save('sample.png')
 
         # # output
         output_url = extract_origin_pathname(upload_url)
-        output = { 'output_url': output_url }
+        output = {
+            'input_url': input_url,
+            'output_url': output_url
+        }
 
         upload_image(upload_url, output_image)
 
